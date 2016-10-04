90eb707208555d06109600f3f9a03414bc5b03b3
yoav freund <yfreund@ucsd.edu>
2016-10-04 13:37:05 -0700

    updated Prob13Part1 and added last_universal/missing_terms.py and unregonized_terms.py

--- hint_database/hint_class/last_Universal/common_subexpressions.py
+++ hint_database/hint_class/last_Universal/common_subexpressions.py
@@ -5,7 +5,7 @@ def check_attempt(params):
     final_pairs=find_matches(params)
     if len(final_pairs)>0:
         for node,value,ans_piece,attempt_piece in final_pairs:
-            if value>10 or value != int(value):
+            if value>20 or value != int(value):
                 if attempt_piece != ans_piece:
                     return 'The sub-expression {0} is correct, it could also be written as {1}'.format(attempt_piece,ans_piece)
                 else:
