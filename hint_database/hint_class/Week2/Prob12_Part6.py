79a77b7e6ba6a1b7824909ee152384398e634b44
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-10-05 13:43:51 -0700

    fix some hint classes

--- hint_database/hint_class/Week2/Prob12_Part6.py
+++ hint_database/hint_class/Week2/Prob12_Part6.py
@@ -14,10 +14,26 @@ class Prob12_Part6:
 		self.att_tree = params['att_tree'] #attempt tree
 		self.ans_tree = params['ans_tree'] #solution tree
 		matches = find_matches(params)
+		matching_node = [m[0] for m in matches]
 
-		if any('R.1' in m for m in matches):
-			return "How many different ways to select 6 heads in the H/T sequence of length 9?","C(9,6)"
-		return "", ""
+		try:
+			#check if the form of the parse tree has the right
+			#shape: an operator and two leafs that correspond to
+			#the operands
+			if len(self.att_tree)==3 and self.att_tree[1][0]=='X' and self.att_tree[2][0]=='X':
+				if 'R.1' in matching_node and 'R.0.0' in matching_node:
+					hint = "The denominator is correct. "
+					return hint + "How many different ways to select 2 heads in the H/T sequence of length 3(order doesn't matter)?","C(3,2)"
+				elif 'R.1' in matching_node:
+					hint = "The denominator is correct. "
+					return hint + "How many different ways to select 2 heads in the H/T sequence of length 3?","C(3,2)"
+				elif 'R.0' in matching_node:
+					hint = "The numerator is correct. "
+					return hint + "What is the number of all possible H/T sequence of length 3?", "2^3"
+			return "", ""
+
+		except Exception:
+			return '',''
 
 	def get_problems(self):
 		self.problem_list = ["Combinatorics/coinProblems/part6"]
