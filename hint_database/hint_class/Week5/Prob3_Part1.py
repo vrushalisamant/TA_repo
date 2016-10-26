9e14ee022fd28434f3e400157815c04f10ca2a60
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-10-26 00:19:44 -0700

    Rename classes

--- hint_database/hint_class/Week5/Prob3_Part1.py
+++ hint_database/hint_class/Week5/Prob3_Part1.py
@@ -1,6 +1,6 @@
 # Make sure you name your file with className.py
 # To test your class make sure you add the local path of hint_class_helpers in your system path
-class Problem3Part5:
+class Prob3_Part1:
 	"""
 	Author: Vrushali Samant
 	Date: 10/24/2016
@@ -12,8 +12,7 @@ class Problem3Part5:
 		self.att_tree = params['att_tree'] #attempt tree
 		self.ans_tree = params['ans_tree'] #solution tree
 
-		return "If the component weight of the normal distribution is .55, what is the weight then of the uniform distrbution?",".45"
-
+		return "Is the spread of the normal component 1. narrow; 2. average; 3. wide? (Enter the number corresponding to the correct answer).", "1."
 
 	def get_problems(self):
 		self.problem_list = ["CumulativeDistributionFunctions/cdf_norm_uni"]
--- hint_database/hint_class/Week5/Prob3_Part4.py
+++ hint_database/hint_class/Week5/Prob3_Part4.py
@@ -1,6 +1,6 @@
 # Make sure you name your file with className.py
 # To test your class make sure you add the local path of hint_class_helpers in your system path
-class Problem3Part1:
+class Prob3_Part4:
 	"""
 	Author: Vrushali Samant
 	Date: 10/24/2016
@@ -12,7 +12,7 @@ class Problem3Part1:
 		self.att_tree = params['att_tree'] #attempt tree
 		self.ans_tree = params['ans_tree'] #solution tree
 
-		return "Is the spread of the normal component 1. narrow; 2. average; 3. wide? (Enter the number corresponding to the correct answer).", "1."
+		return "If the uniform, non-curved component extends from [1,4], what is its b-value?", "4"
 
 	def get_problems(self):
 		self.problem_list = ["CumulativeDistributionFunctions/cdf_norm_uni"]
