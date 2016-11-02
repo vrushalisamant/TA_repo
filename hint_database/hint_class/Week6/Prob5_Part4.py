27691f2b43039180572befc2dfd8260ce81915ce
Tony Lim <atomictheorist@gmail.com>
2016-11-01 21:13:53 -0700

    Add Week6 Prob 5 Hints

---
+++ hint_database/hint_class/Week6/Prob5_Part4.py
@@ -0,0 +1,31 @@
+from hint_class_helpers.find_matches import find_matches
+
+class Prob1_Part1:
+    """
+    Author: <Tony Salim>
+    Date: <11-1-2016>
+    """
+
+    def check_attempt(self, params):
+        self.attempt = params['attempt'] #student's attempt
+        self.answer = params['answer'] #solution
+        self.att_tree = params['att_tree'] #attempt tree
+        self.ans_tree = params['ans_tree'] #solution tree
+
+        matches = find_matches(params)
+
+        try:
+            for m in matches:
+                if ('R.0' not in m) or ('R.1' not in m):
+                    hint='To solve for k, express k in terms of expectation, which value you have from previous part.'
+            if len(hint)>0:
+                return hint , ''
+            else:
+                return '',''
+
+        except Exception:
+            return '',''
+
+    def get_problems(self):
+        self.problem_list = [ "MarkovChebyshev/RiggedDice/part4" ]
+        return self.problem_list
